e31344a0ac983c081e53cb8fa47610ceabeffa1d
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-25 23:25:33 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -8,11 +8,11 @@ switch (action.type) {
   case 'ADD_TODO':
     console.log({ todos:
       state.todos.concat(action.payload.text) });
-      return { todos: 
+      return { todos:
       state.todos.concat(action.payload.text) };
-      
+
       default:
       return state;
-      
+
 }
 }
